d39cf6258540721fdbb2d8e09a6b3482e6e4dc94
Leyan Buirat <lyanbyrat53@gmail.com>
2024-09-11 00:39:18 +0300

    initial commit

---
+++ all file/main.js
@@ -0,0 +1,10 @@
+let img = document.querySelector('.img');
+let container = document.querySelector('.container');
+
+function phones(phone) {
+    img.src = phone;
+}
+
+function back_ground(color) {
+    container.style.background = color;
+}
